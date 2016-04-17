a27f83183f62531b8b65bfae4220097a33e9571a
Robert Zurga <bitbucket@zurga.com>
2016-04-17 15:11:25 -0500

    Add pv

---
+++ recipes/pv.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: linux_tools
+# Recipe:: pv
+#
+# Copyright 2016, vagrant@zurga.com
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "pv" do
+  action :install
+end
