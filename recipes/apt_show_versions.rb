203280559903370ffc33b76cd75eaefc69c4865a
Robert Zurga <bitbucket@zurga.com>
2016-04-19 03:37:52 -0500

    Add apt-show-versions

---
+++ recipes/apt_show_versions.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: linux_tools
+# Recipe:: apt_show_versions
+#
+# Copyright 2016, vagrant@zurga.com
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "apt-show-versions" do
+  action :install
+end
