89e9a78eac94bd5a9a2843d221e534779da272e7
Robert Zurga <bitbucket@zurga.com>
2016-04-24 10:45:46 -0500

    Add QA Cert fix for Oracle Java download

--- recipes/ca_certificates.rb
+++ recipes/ca_certificates.rb
@@ -10,3 +10,14 @@
 package "ca-certificates" do
   action :install
 end
+
+# Fix CA Certs issue for downloading the Java SDK
+bash "CA Certs" do
+    user "root"
+    action :run
+    not_if { ::File.directory?("/etc/pki/tls/certs") }
+    code <<-EOD
+        mkdir -p /etc/pki/tls/certs
+        cp /etc/ssl/certs/ca-certificates.crt /etc/pki/tls/certs/ca-bundle.crt
+    EOD
+end
