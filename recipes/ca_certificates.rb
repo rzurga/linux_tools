abfcbb2dda4691739d472c70950c7dbcfb9a0376
Robert Zurga <bitbucket@zurga.com>
2016-04-24 09:54:21 -0500

    Added ca-certificates

---
+++ recipes/ca_certificates.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: linux_tools
+# Recipe:: ca_certificates
+#
+# Copyright 2016, vagrant@zurga.com
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "ca-certificates" do
+  action :install
+end
